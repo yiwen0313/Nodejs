0dd073b0be69c6b29256f27cd439e1c40bf6d05c
yiwen0313 <2469119402@qq.com>
2021-09-17 15:45:57 +0800

    node搜索包的流程 学习完成

--- CODE/01_基础/05.module.js
+++ CODE/01_基础/05.module.js
@@ -1,7 +1,7 @@
 /*
 *  exports 和 module.exports
 *     - 通过exports只能使用.的方式来向外暴露内部变量
-*          export.xxx = xxx;
+*          exports  .xxx = xxx;
 *     - 而module.exports既可以通过.的形式，也可以直接赋值
 *          module.exports.xxx = xxx;
 *          module.exports = {};
