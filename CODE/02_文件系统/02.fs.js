8ae4efe45fb2401d988b0be0ad756148298a220b
yiwen0313 <2469119402@qq.com>
2021-09-17 19:02:36 +0800

    同步文件写入及异步文件写入 学习完成

---
+++ CODE/02_文件系统/02.fs.js
@@ -0,0 +1,42 @@
+/*
+* 文件系统 (File System)
+*    - 简单来说，就是通过Node来操作系统中的文件
+*    - 使用文件系统，需要先引入fs模块，fs是核心模块，直接引入不需要下载
+*
+* 文件的写入
+*    - 手动操作步骤
+*       1.打开文件
+*           fs.openSync(path[, flags[, mode]])
+*              - path  要打开文件的路路径
+*              - flags 打开文件要做的操作的类型
+*                  r  只读的
+*                  w  可写的
+*              - mode   设置文件的操作权限，一般不填
+*           返回值：
+*              - 该方法会返回一个文件的描述符作为结果 ，可以通过改文件描述符来对文件进行各种操作
+*
+*       2.向文件中写入内容
+*           fs.writeSync(fd, string[, position[, encoding]])
+*              - fd       文件的描述符，需要传递要写入文件的描述符
+*              - string   要写入的内容
+*              - position 写入的起始位置 (一般不传)
+*              - encoding 写入的编码，默认UTF-8 (一般不传)
+*
+*       3.保存并关闭文件
+*           fs.closeSync(fd)
+*              - fd  要关闭的文件的描述符
+*
+* */
+
+var fs = require("fs");
+
+// 打开文件
+var fd = fs.openSync("hello.txt", "w");
+
+// 向文件中写入内容
+fs.writeSync(fd, "今天天气真不错", 2)
+
+//关闭文件
+fs.closeSync(fd);
+
+// console.log(fd);
